f1413b7484437857edf0e931865c794a69473bbe
Guilherme Theodoro <gui.theodoro2010@gmail.com>
2026-08-10 14:28:56 -0500

    init: new gem project setup

---
+++ lib/active_jobject.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "active_jobject/model"
+require_relative "active_jobject/request"
+require_relative "active_jobject/version"
+
+module ActiveJobject
+  class Error < StandardError; end
+  # Your code goes here...
+end
